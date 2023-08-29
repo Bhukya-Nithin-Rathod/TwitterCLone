c936a89511def830193e01480a45ab79be86530b
Bhukya-Nithin-Rathod <105960383+Bhukya-Nithin-Rathod@users.noreply.github.com>
2023-08-29 10:13:12 +0530

    Update TweetBox.js

--- TweetBox.js
+++ TweetBox.js
@@ -11,8 +11,8 @@ function TweetBox() {
     e.preventDefault();
 
     db.collection("posts").add({
-      displayName: "Rafeh Qazi",
-      username: "cleverqazi",
+      displayName: "Bhukya Nithin Rathod",
+      username: "strongpassword123",
       verified: true,
       text: tweetMessage,
       image: tweetImage,
